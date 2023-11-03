3a350d847cebbe589c11ea3b185512e0e4ed76e1
Thang <you@example.com>
2023-11-02 22:15:23 -0700

    loading svg animation for fetching post

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -5,11 +5,21 @@ const Posts = ({ setUpdatingID }) => {
   // get the posts state from the store
   const { posts, loading, error } = useSelector((state) => state.posts);
 
-
   return (
     <>
-      {loading && <h1>Loading...</h1>}
-      {!posts.length && <p className="italic text-3xl">... No post available yet</p>}
+      {loading && (
+        <div className='flex gap-4 flex-wrap justify-center items-center'>
+          <img
+            className='w-20 h-20 mt-80 animate-spin'
+            src='https://www.svgrepo.com/show/448500/loading.svg'
+            alt='Loading icon'
+          />
+        </div>
+      )}
+
+      {!posts.length && !loading && (
+        <p className='italic text-3xl'>... No post available yet</p>
+      )}
 
       {!loading && !error && posts && (
         <div className='grid grid-cols-2 gap-4 w-full'>
