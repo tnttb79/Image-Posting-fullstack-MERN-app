a254d97741bcfc16f412674c57f463b5267cb555
Thang <you@example.com>
2023-11-15 12:43:27 -0700

    deployment

--- server/index.js
+++ server/index.js
@@ -2,33 +2,35 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
-import authRoutes from "./routes/auth.js"
-import 'dotenv/config'
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import authRoutes from "./routes/auth.js";
+import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
+const corsOptions = { origin: "http://localhost:5173"}
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 app.use(express.json({ extended: true, limit: "30mb" }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 // server static assests in images folder so multer can save img to it
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use("/posts", postRoutes);
 app.use("/users", authRoutes);
 
 mongoose
-  .connect(process.env.MONGODB_URL)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to the database");
-    app.listen(process.env.PORT || 3000, () => console.log(`Server is running on port: ${process.env.PORT || 3000}`));
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
   })
   .catch((err) => {
-    console.error(`Error connecting to the database. ${err}`);
+    console.log(`Error connecting to the database. ${err}`);
   });
